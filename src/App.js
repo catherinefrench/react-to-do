1abc7e8cf81530472ccb55a5101f81745ed775bc
Catherine French <catherine.french@me.com>
2022-01-22 10:04:47 -0500

    Adds unique task IDs and updates taskNoun

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
-
+import { nanoid } from "nanoid";
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
@@ -16,9 +16,11 @@ function App(props) {
     )
   );
   function addTask(name) {
-    const newTask = { id: "id", name: name, completed: false };
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
   }
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -28,7 +30,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
